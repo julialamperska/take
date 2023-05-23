ce4d4c62dfd1782f681e785d994df54f309701b3
Julia Lamperska <68638686+julialamperska@users.noreply.github.com>
2023-05-24 00:07:19 +0200

    TAKE - lab 8

---
+++ lab8/CompanyApi/CompanyApi/Controllers/DepartmentsController.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CompanyApi.Models;
+
+namespace CompanyApi.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        private readonly CompanyDbContext _context;
+
+        public DepartmentsController(CompanyDbContext context)
+        {
+            _context = context;
+        }
+
+        private static DepartmentDTO DepartmentToDTO(Department department) => new DepartmentDTO
+        {
+            DepartmentId = department.DepartmentId,
+            Name = department.Name
+        };
+        private static Department DTOToDepartment(DepartmentDTO departmentDTO) => new Department
+        {
+            DepartmentId = departmentDTO.DepartmentId,
+            Name = departmentDTO.Name
+        };
+
+        // Get all
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<DepartmentDTO>>> GetDepartments()
+        {
+            return await _context.Departments
+                .Select(e => DepartmentToDTO(e))
+                .ToListAsync();
+        }
+
+        // Get 1
+        [HttpGet("{id}")]
+        public async Task<ActionResult<DepartmentDTO>> GetDepartment(int id)
+        {
+            var department = await _context.Departments.FindAsync(id);
+
+            if (department == null)
+            {
+                return NotFound();
+            }
+
+            return DepartmentToDTO(department);
+        }
+
+        // Post
+        [HttpPost]
+        public async Task<ActionResult<DepartmentDTO>> PostDepartment(DepartmentDTO departmentDTO)
+        {
+            _context.Departments.Add(DTOToDepartment(departmentDTO));
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetDepartment", new { id = departmentDTO.DepartmentId }, departmentDTO);
+        }
+
+
+        // Put
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutDepartment(int id, DepartmentDTO departmentDTO)
+        {
+            if (id != departmentDTO.DepartmentId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(DTOToDepartment(departmentDTO)).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!DepartmentExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // Delete
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteDepartment(int id)
+        {
+            var department = await _context.Departments.FindAsync(id);
+            if (department == null)
+            {
+                return NotFound();
+            }
+
+            _context.Departments.Remove(department);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool DepartmentExists(int id)
+        {
+          return (_context.Departments?.Any(e => e.DepartmentId == id)).GetValueOrDefault();
+        }
+    }
+}
